61e3b89e8398e78236b84328024b14ae0b95f905
david karapetyan <dkarapetyan@siftscience.com>
2017-01-08 14:21:42 -0800

    factor out evented listener into a class

--- file-tailer.dart
+++ file-tailer.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'dart:async';
-import 'dart:convert';
 
 // sometimes delete does not mean deleted so we have to
 // do an extra check when we receive a delete event
@@ -20,6 +19,9 @@ Stream<FileSystemEvent> saneEvents(File f) async* {
   }
 }
 
+// Keep the local state of the file we are tracking and allow operations
+// to be performed on said file like reading in a non-blocking manner,
+// resetting read position, etc.
 class Context {
 
   final String filename;
@@ -37,40 +39,26 @@ class Context {
     recentPosition = start;
   }
 
-  Future<List<int>> read(int size) {
-    return opened.read(size);
-  }
+  Future<List<int>> read(int size) => opened.read(size);
 
-  bool positionMismatch() {
-    return recentPosition != opened.lengthSync();
-  }
+  get positionMismatch => recentPosition != opened.lengthSync();
 
-  void setPositionSync(int position) {
-    opened.setPositionSync(position);
-  }
+  updateRecentPosition() { recentPosition = opened.positionSync(); }
 
-  void updateRecentPosition() {
-    recentPosition = opened.positionSync();
-  }
+  get position => opened.positionSync();
 
-  Future<int> position() {
-    return opened.position();
-  }
+  set position(int position) { opened.setPositionSync(position); }
 
 }
 
-Future main() async {
-  String f = 't.log';
-  // nothing to do if the file doesn't exist
-  if (!FileSystemEntity.isFileSync(f)) {
-    return 0;
-  }
-  // the file we are interested in watching
-  final Context context = new Context(f);
-  // seek to the starting position
-  print('Starting length: ${context.start}');
-  // the logic for what we do when there is a modification event
-  final modificationAction = (final FileSystemEvent ev) async {
+// watches the events on the file and performs contextual operations
+class EventedTailer {
+
+  final Context context;
+
+  EventedTailer(final this.context);
+
+  modificationAction(final FileSystemEvent ev) async {
     if ((ev as FileSystemModifyEvent).contentChanged) {
       List<int> read = await context.read(200);
       // we tried to read but got nothing so maybe truncated
@@ -82,9 +70,9 @@ Future main() async {
         // and position will match but we won't know that we need
         // to reset the position. i'm going to assume this is very unlikely
         // because i don't know how to avoid it.
-        if (context.positionMismatch()) {
+        if (context.positionMismatch) {
           // reset to 0 and retry
-          context.setPositionSync(0);
+          context.position = 0;
           read = await context.read(200);
         }
         // Note: the above logic can fail in an interesting way.
@@ -102,31 +90,46 @@ Future main() async {
     else { // TODO: content was not modified so what should we do?
 
     }
-  };
-  saneEvents(context.tailee).listen((final FileSystemEvent ev) async {
+  }
+
+  // start listening for file events and performing contextual actions
+  start() {
+    saneEvents(context.tailee).listen((final FileSystemEvent ev) async {
       print('Event: ${ev}');
       // dispatch on the event type
       switch (ev.type) {
-        // can't do anything with a deleted file
+      // can't do anything with a deleted file
         case FileSystemEvent.DELETE:
           return 0;
-        // similar to above we just give up and wait to be restarted
+      // similar to above we just give up and wait to be restarted
         case FileSystemEvent.MOVE:
           return 0;
-        // go to beginning
+      // go to beginning
         case FileSystemEvent.CREATE:
-          context.setPositionSync(0);
+          context.position = 0;
           break;
-        // file was modified so we should try to read
+      // file was modified so we should try to read
         case FileSystemEvent.MODIFY:
           modificationAction(ev);
           break;
-        // we covered all event cases above so this can't happen
+      // we covered all event cases above so this can't happen
         default:
           throw "unreachable";
       }
-      final int endPosition = await context.position();
-      print('Position: ${endPosition}');
+      print('Position: ${context.position}');
     });
   }
+
+}
+
+Future main() async {
+  String f = 't.log';
+  // nothing to do if the file doesn't exist
+  if (!FileSystemEntity.isFileSync(f)) {
+    return 0;
+  }
+  // the file we are interested in watching
+  final Context context = new Context(f);
+  final EventedTailer eventedTailer = new EventedTailer(context);
+  eventedTailer.start();
 }
